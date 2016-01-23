e0ff43c13f5152ffe10e0cdb01e3a6d3b908be06
Luan Nguyen Thanh <tklarryonline@gmail.com>
2016-01-23 18:06:28 +0700

    [r] Add help text for the command

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from disk_driller.driller import DiskDriller
 @click.command()
 @click.argument('directory', default=os.getcwd())
 def main(directory):
+    '''
+    Simple CLI program that drills your directory and sees how fast it writes the file.
+    '''
     speed = DiskDriller.drill(directory)
 
     print('Speed: {speed:.2f} MBs per second'.format(
